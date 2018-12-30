9bb4a39697ba7d92823b8ba4b92e94f7a74f2912
marcinstenka <mastenka@wp.pl>
2018-12-30 20:56:08 +0100

    Adding initial structure of page

---
+++ js/test.js
@@ -0,0 +1,13 @@
+
+$(document).ready(function(){
+  $( "#klik" ).click(function() {
+ 
+  $( "#zegar" ).animate({
+    height: "toggle"
+  }, 1000, function() {
+    // Animation complete.
+  });
+});
+
+});
+ 
